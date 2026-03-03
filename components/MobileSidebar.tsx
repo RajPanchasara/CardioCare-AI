a438133cd96d2955d98571ccfba3c0223b15b282
RajPanchasara <hp.panchasara27@gmail.com>
2026-03-03 08:15:08 +0530

    Refactor MobileSidebar component structure

--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -89,8 +89,9 @@ export default function MobileSidebar({ isOpen, onClose }: MobileSidebarProps) {
 
                     {/* Secondary */}
                     <Link href="/guidance" className={secondary("/guidance")} onClick={onClose}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                             <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z" />
+                            <path d="M9 12l2 2 4-4" />
                         </svg>
                         Health Guidance
                     </Link>
@@ -145,4 +146,5 @@ export default function MobileSidebar({ isOpen, onClose }: MobileSidebarProps) {
         </>
     );
 }
+
 
